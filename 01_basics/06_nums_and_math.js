8f3b39ef3e1f3ad8bece6dc13227e637352ac865
pankajpn <pankajnargesh786@gmail.com>
2023-10-24 21:54:07 +0530

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,31 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const otherNumber = 23.8899
+
+console.log(otherNumber.toPrecision(3))
+console.log(otherNumber.toPrecision(2))
+
+// +++++++++++++++++ Maths ++++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.9))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.9))
+console.log(Math.PI)
+console.log(Math.random())
+
+console.log(Math.min(4,3,2,2))
+console.log(Math.max(4,3,2,2))
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1) + min))
