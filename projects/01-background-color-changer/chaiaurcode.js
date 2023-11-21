838a69ce30e26c355efeac25957c114b0cb19f03
pankajpn <pankajnargesh786@gmail.com>
2023-11-21 10:20:32 +0530

    bg color switching

---
+++ projects/01-background-color-changer/chaiaurcode.js
@@ -0,0 +1,15 @@
+console.log("Pankaj");
+
+const buttons = document.querySelectorAll('.button')
+console.log(buttons);
+
+const body = document.querySelector('body')
+
+buttons.forEach(button => {
+    console.log(button)
+    button.addEventListener('click', function(e){
+        console.log(e)
+        console.log(e.target)
+        body.style.backgroundColor = e.target.id
+    })
+});
