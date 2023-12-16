51e98cbe5955089355f30f9d53c90372b25f6181
pankajpn <pankajnargesh786@gmail.com>
2023-12-16 17:54:46 +0530

    digital clock

---
+++ projects/03-DigitalClock/chaiaurcode.js
@@ -0,0 +1,9 @@
+console.log('Pankaj');
+
+const clock  = document.getElementById('clock');
+
+setInterval(function(){
+    let date = new Date();
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
